e23b6d81189afad4c4303f240842e3c02829fdaf
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-06 00:18:42 -0300

    ft_isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 23:21:34 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/06 00:14:13 by lbiasuz          ###   ########.fr       */
+/*   Created: 2022/04/06 00:14:34 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/04/06 00:18:11 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return (!(c < 32 || c > 126));
+}
