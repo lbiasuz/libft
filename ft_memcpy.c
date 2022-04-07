92a3f959e014f276668d8b3ff0c06855d7d169fb
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-07 20:30:42 -0300

    memcpy memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 23:21:34 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/07 20:26:23 by lbiasuz          ###   ########.fr       */
+/*   Created: 2022/04/07 19:42:48 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/04/07 19:52:12 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, char c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-#endif
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((char *) dest + i) = *((char *) src + i);
+		i++;
+	}
+	return (dest);
+}
