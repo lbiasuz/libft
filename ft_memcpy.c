a8df43a06698f411e77f2b9a4f1fd050ea184478
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-07 23:16:01 -0300

    testes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:42:48 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/07 19:52:12 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/07 23:13:51 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	i;
+	unsigned int	i;
+	char			*d;
+	char			*s;
 
 	i = 0;
+	d = (char *) dest;
+	s = (char *) src;
 	while (i < n)
 	{
-		*((char *) dest + i) = *((char *) src + i);
+		*(d + i) = *(s + i);
 		i++;
 	}
 	return (dest);
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,26 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 20:14:19 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/07 20:26:14 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/07 23:03:45 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int	i;
+	unsigned int	i;
+	char			*d;
+	char			*s;
 
 	i = -1;
-	if (dest > src)
+	d = (char *) dest;
+	s = (char *) src;
+	if (d > s)
 		while (n--)
-			*((char *) dest + n) = *((char *) src + n);
+			*(d + n) = *(s + n);
 	else
 		while (++i < n)
-			*((char *) dest + i) = *((char *) src + i);
+			*(d + i) = *(s + i);
 	return (dest);
 }
