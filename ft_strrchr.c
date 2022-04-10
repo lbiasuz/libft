30dd8cb4ad274112629cb4d774de205e9fae9efc
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-09 22:13:15 -0300

    strrchr fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 19:57:24 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/09 20:57:11 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/09 22:01:17 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strrchr(const char *s, int c)
 	char	*a;
 
 	a = s + ft_strlen(s);
-	while (*s && s != c)
-		s--;
-	if (*s == c)
-		return (s);
+	while (a != s && a != c)
+		a--;
+	if (*a == c)
+		return (a);
 	return (NULL);
 }
