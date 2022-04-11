71c9650d3a51880800da6c8dcfdc6aeb37301aee
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-10 22:34:03 -0300

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 22:15:38 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/04/10 22:33:45 by lbiasuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	n = 1;
+	while (isspace(*nptr))
+		nptr++;
+	if (*nptr == '-')
+		n = -1;
+	if (!ft_isdigit(*nptr))
+		nptr++;
+	while (ft_isdigit(*nptr))
+	{
+		i = (i * 10) + (*nptr - 48);
+		nptr++;
+	}
+	return (i * n);
+}
+
+static int	isspace(char c)
+{
+	if (c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
+		return (1);
+	return (0);
+}
