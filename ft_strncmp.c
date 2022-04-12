cec94c52c497ac48e1b8f0d41e877caf8c3f344d
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-11 23:15:47 -0300

    teste strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,16 +6,18 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:01:20 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/09 21:07:16 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/11 23:15:27 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n && s1[i] == s2[i])
+	while (s1[i] && s2[i] && i < n - 1 && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 23:21:34 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/10 22:39:41 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/11 23:03:29 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, char c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, char c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*strnstr(const char *big, const char *little, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*calloc(size_t nmemb, size_t size);
 
