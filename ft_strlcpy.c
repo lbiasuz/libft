7261e0af8ee623ef7741b0687d445bc029058d61
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-13 21:40:37 -0300

    correções testes externos

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 23:20:45 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/08 22:21:23 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/13 21:38:09 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	unsigned int		i;
-	int					src_l;
+	unsigned int		src_l;
 
 	i = 0;
 	src_l = ft_strlen(src);
-	while (i < size && src[i])
+	if (!size)
+		return (src_l);
+	while (i < size - 1 && src[i])
 	{
 		dest[i] = src[i];
 		i++;
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:01:20 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/11 23:15:27 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/13 20:02:14 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
+	if (!n)
+		return (0);
 	while (s1[i] && s2[i] && i < n - 1 && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
