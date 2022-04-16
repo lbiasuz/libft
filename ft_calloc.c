0f2d4955e928021f8212cff19735e7fdb20a9b7c
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-16 15:04:48 -0300

    norminette

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 22:47:16 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/13 22:04:17 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/16 15:01:23 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned int	i;
 	char			*s;
-	
+
 	i = 0;
 	if (nmemb > __SIZE_MAX__ / size)
 		return (NULL);
 	s = malloc((nmemb * size));
 	if (!s)
 		return (NULL);
-	while (i < (nmemb * size) -1)
+	while (i < (nmemb * size) - 1)
 	{
 		s[i] = '\0';
 		i++;
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 23:49:52 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/16 02:19:10 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/16 15:00:18 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,75 +28,13 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	if (size == 0)
 		return (sl);
 	else if (size < dl)
-		return (size+sl);
-	while (*(src+i) != '\0' && off < size - 1)
+		return (size + sl);
+	while (*(src + i) != '\0' && off < size - 1)
 	{
-		*(dest+off) = *(src+i);
+		*(dest + off) = *(src + i);
 		i++;
 		off++;
 	}
-	*(dest+off) = '\0';
-	return (dl+sl);
+	*(dest + off) = '\0';
+	return (dl + sl);
 }
-
-// int	main (void)
-// {
-// 	char dest[30];
-// 	char *src = (char *)"AAAAAAAAA";
-	
-// 	dest[0] = 'B';
-// 	dest[1] = '\0';
-// 	printf("FT: %ld \n", ft_strlcat(dest, src, 0));
-// 	printf("String: %s \n", dest);
-// 	dest[0] = 'B';
-// 	printf("FT: %ld \n", ft_strlcat(dest, src, 1));
-// 	printf("String: %s \n", dest);
-// 	memset(dest, 'B', 4);
-// 	printf("FT: %ld \n", ft_strlcat(dest, src, 3));
-// 	printf("String: %s \n", dest);
-// 	memset(dest, 'B', 4);
-// 	printf("FT: %ld \n", ft_strlcat(dest, src, 6));
-// 	printf("String: %s \n", dest);
-// 	memset(dest, 'C', 15);
-// 	printf("FT: %ld \n", ft_strlcat(dest, src, 17));
-// 	printf("String: %s \n", dest);
-// }
-
-// size_t	ft_strlcat(char *dest, const char *src, size_t size)
-// {
-// 	unsigned int	di;
-// 	unsigned int	si;
-
-// 	si = 0;
-// 	if (size == 0)
-// 		return (ft_strlen(src));
-// 	i = ft_strlen(dest) + 1;
-// 	// if (ft_strlen(dest) < size)
-// 	// 	size++;
-// 	while (i <= size + 1 && src[si])
-// 	{
-// 		dest[i] = src[si];
-// 		si++;
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (ft_strlen(dest)+ft_strlen(src));
-// }
-
-
-// size_t	ft_strlcat(char *dest, const char *src, size_t size)
-// {
-// 	unsigned int	i;
-
-// 	i = ft_strlen(dest);
-// 	size -= i;
-// 	while (size && *src)
-// 	{
-// 		dest[i] = *src;
-// 		i++;
-// 		src++;
-// 		size--;
-// 	}
-// 	dest[i] = '\0';
-// 	return (i);
-// }
