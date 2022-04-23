d085b08f21b8550091a504892a4889b1ef0ffe8e
Monaco-Leonardo <leonardo.biasuz@monacobr.com.br>
2022-04-23 09:06:48 -0300

    correções strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 20:01:05 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/12 22:58:00 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/23 09:06:21 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char	*ft_strdup(const char *s)
 	unsigned int	i;
 	char			*d;
 
-	i = ft_strlen(s);
-	d = (char *) malloc(i);
+	i = ft_strlen(s) + 1;
+	d = (char *) ft_calloc(i, sizeof(char));
 	if (!d)
 		return (NULL);
 	ft_strlcpy(d, s, i);
-	if (ft_strlen(d) != i)
+	if (ft_strlen(d) != i -1)
 		return (NULL);
 	return (d);
 }
