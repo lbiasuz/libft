cbb6d17fde94c89688e179d5561b07b4ab8bd4a9
Monaco-Leonardo <leonardo.biasuz@monacobr.com.br>
2022-04-23 15:08:16 -0300

    ft_putendl

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 23:21:34 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/23 15:04:43 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/23 15:06:16 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
