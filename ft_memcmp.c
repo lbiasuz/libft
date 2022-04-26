302c1597e8e05cefef85f116f5c0192697c9d6fe
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-25 23:14:42 -0300

    correção memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:37:57 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/16 15:18:51 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/25 23:13:53 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char			*aux1;
-	char			*aux2;
+	unsigned char	*aux1;
+	unsigned char	*aux2;
 	unsigned int	i;
 
 	i = 0;
-	aux1 = (char *) s1;
-	aux2 = (char *) s2;
+	aux1 = (unsigned char *) s1;
+	aux2 = (unsigned char *) s2;
 	if (!n)
 		return (0);
 	while (i < n - 1 && aux1[i] == aux2[i])
 		i++;
-	return (aux2[i] - aux1[i]);
+	return (aux1[i] - aux2[i]);
 }
