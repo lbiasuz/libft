5c4d16e3bf7d0c085a1ea8732de8437f4139633c
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-04-25 22:56:59 -0300

    correção strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:01:20 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/04/13 20:02:14 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/04/25 22:56:37 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] && s2[i] && i < n - 1 && s1[i] == s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
